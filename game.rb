9659b3ff24cee763a517e9de5eb1cf56bc85befd
TangibleDream <darclan28@yahoo.com>
2014-10-18 17:44:05 -0500

    Character refactor

--- game.rb
+++ game.rb
@@ -17,26 +17,8 @@ class Game
     pn = gets.chomp
     character = Character.new
     character.set_name(pn)
-    character.set_maximum_hit_points(10)
-    character.set_hit_points(10)
-    character.set_money(0)
-    character.set_weapon(0)
-    character.set_level(1)
-    savechar(character)
-    display_char(character)
-  end
-
-  def display_char(char)
-    puts '****************'
-    puts "Name:   #{char.get_name}"
-    puts "HP:     #{char.get_hit_points} of #{char.get_maximum_hit_points}"
-    puts "Level:  #{char.get_level}"
-    puts "Weapon: #{char.weaponname(char.get_weapon)}"
-    puts "Armor:  #{char.armorname(char.get_armor)}"
-    puts "Shield: #{char.shieldname(char.get_shield)}"
-    puts "Coin:   #{char.get_money}"
-    puts "Exp:    #{char.get_experience}"
-    puts '****************'
+    character.save
+    character.display
   end
 
   def dungeon(char)
@@ -51,29 +33,10 @@ class Game
       exitdungeon = 0
     end
   end
-
-  def get_character
-    f = File.open('c.json', 'r')
-    config = JSON.load(f)
-  end
-
   def get_monster
     f = File.open('m.json', 'r')
     config = JSON.load(f)
   end
-
-  def loadchar(char)
-    char.set_name(get_character['character']['name'])
-    char.set_maximum_hit_points(get_character['character']['mhp'])
-    char.set_hit_points(get_character['character']['hp'])
-    char.set_level(get_character['character']['level'])
-    char.set_weapon(get_character['character']['weapon'])
-    char.set_armor(get_character['character']['armor'])
-    char.set_shield(get_character['character']['shield'])
-    char.set_money(get_character['character']['coin'])
-    char.set_experience(get_character['character']['exp'])
-  end
-
   def loadmonster(mon, char)
     randomlevel = rand(10)+1
     case
@@ -97,7 +60,7 @@ class Game
   def run
     exit = false
     char = Character.new
-    loadchar(char)
+    char.loadchar
     system "cls"
     while !exit
       puts "Location: Crossroads"
@@ -118,7 +81,7 @@ class Game
           shopping(char)
         when 3
           system "cls"
-          display_char(char)
+          char.display
         when 4
           puts "See you later!"
           exit = true
@@ -126,25 +89,6 @@ class Game
           puts "Please enter a given option"
       end
     end
-  end
-
-  def savechar(char)
-    outfile = open('c.json', 'w')
-    outfile.write ("{\n")
-    outfile.write ("    \"character\" :\n")
-    outfile.write ("    {\n")
-    outfile.write ("        \"name\" : \"#{char.get_name}\",\n")
-    outfile.write ("        \"mhp\" : #{char.get_maximum_hit_points},\n")
-    outfile.write ("        \"hp\" : #{char.get_hit_points},\n")
-    outfile.write ("        \"level\" : #{char.get_level},\n")
-    outfile.write ("        \"weapon\" : #{char.get_weapon},\n")
-    outfile.write ("        \"armor\"  : #{char.get_armor},\n")
-    outfile.write ("        \"shield\" : #{char.get_shield},\n")
-    outfile.write ("        \"coin\" : #{char.get_money},\n")
-    outfile.write ("        \"exp\" : #{char.get_experience}\n")
-    outfile.write ("    }\n")
-    outfile.write ("}\n")
-    outfile.close
   end
 
   def shopping(char)
@@ -183,7 +127,7 @@ class Game
                 when 1
                   char.set_money(char.get_money - health_cost)
                   char.set_hit_points(char.get_maximum_hit_points)
-                  savechar(char)
+                  char.save
                 when 2
                   healexit = 0
               end
@@ -196,7 +140,7 @@ class Game
                 when 1
                   char.set_hit_points(char.get_hit_points + char.get_money * 2)
                   char.set_money(0)
-                  savechar(char)
+                  char.save
                 when 2
                   healexit = 0
               end
